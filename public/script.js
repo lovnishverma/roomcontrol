c9559e05dfa7892042909f5e6e3309d79806a771
Glitch (mqttnodejs) <none>
2023-08-26 19:20:08 +0000

    Rewound to commit 456734a9ddc6f74b1be405cd3ff34d7cd85f1075 + 0 edits

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 // Connect to the WebSocket server
 const socket = io();
 
-// Get a reference to the audio element
-const clickSound = document.getElementById('clickSound');
-
 // Listen for status updates
 socket.on('statusUpdate', function (status) {
   updateAppStatus(status); // Call the function to update status and toggle switch
@@ -24,10 +21,6 @@ $(document).ready(function () {
   $('#toggleSwitch').change(function () {
     const isChecked = $(this).is(':checked');
     const command = isChecked ? '1' : '0';
-
-    // Play the sound when the switch is toggled
-    clickSound.play();
-
     $.post('/send-command', { command }, function (data) {
       console.log(data);
 
