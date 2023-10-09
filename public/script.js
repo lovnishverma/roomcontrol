41b6d5bd19a59eab3303b60b913039c7635dfbd0
Glitch (olivine-weak-green) <none>
2023-10-09 18:03:50 +0000

    ✂️🍈 Checkpoint ./public/script.js:57629255/533

--- public/script.js
+++ public/script.js
@@ -65,13 +65,15 @@ const toggleSwitch = document.getElementById('toggleSwitch');
                 .then(response => {
                     if (!response.ok) {
                         throw new Error(`HTTP error! Status: ${response.status}`);
-                      speak(`The app status is currently ${state}`);
+                      
                     }
                     return response.text();
                 })
                 .then(data => {
                     console.log(`App state changed to ${state}`);
+              
                     // Play click sound when the switch is toggled
+                    speak(`The app status is turned ${state}`);
                     audio.play();
                 })
                 .catch(error => {
