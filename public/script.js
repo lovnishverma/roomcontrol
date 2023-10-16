eb4aa40f52649bb133d39585ea48b18128811c5a
Glitch (olivine-weak-green) <none>
2023-10-16 07:16:33 +0000

    🏐💨 Checkpoint ./public/script.js:57629255/10638

--- public/script.js
+++ public/script.js
@@ -1,55 +1,4 @@
 $(document).ready(function () {
-  // Initialize the SpeechRecognition API
-  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-  recognition.continuous = true;
-
-  // Get references to the toggleSwitch and audio elements
-  const toggleSwitch = document.getElementById('toggleSwitch');
-  const audio = document.getElementById('audio');
-  const startButton = document.getElementById('startVoiceRecognition');
-  const appStatusText = document.getElementById('appStatus');
-
-  // Variables to keep track of recognition status
-  let isRecognitionActive = false;
-
-  // Handle continuous recognition errors
-  recognition.onerror = function (event) {
-    console.error('Voice recognition error:', event.error);
-  };
-
-  // Handle recognition start
-  recognition.onstart = function () {
-    isRecognitionActive = true;
-    startButton.textContent = 'Stop Voice Recognition';
-    console.log('Voice recognition started');
-  };
-
-  // Handle recognition end
-  recognition.onend = function () {
-    isRecognitionActive = false;
-    startButton.textContent = 'Start Voice Recognition';
-    console.log('Voice recognition ended');
-  };
-
-  // Toggle voice recognition
-  function toggleRecognition() {
-    if (isRecognitionActive) {
-      recognition.stop();
-    } else {
-      recognition.start();
-    }
-  }
-
-  // Add click event listener to the "Start/Stop Voice Recognition" button
-  startButton.addEventListener('click', toggleRecognition);
-
-  // Handle page visibility changes
-  document.addEventListener('visibilitychange', () => {
-    if (document.hidden && isRecognitionActive) {
-      recognition.stop();
-    }
-  });
-
   // Connect to the WebSocket server
   const socket = io();
 
@@ -59,29 +8,56 @@ $(document).ready(function () {
   // Load the audio
   clickSound.load();
 
-  // Handle continuous recognition results
-  recognition.onresult = function (event) {
-    const transcript = event.results[event.results.length - 1][0].transcript.toLowerCase();
-    console.log('Voice input:', transcript);
+  // Function to update app status and toggle switch
+  function updateAppStatus(appStatus) {
+    $('#appStatus').text('App Status: ' + appStatus);
+    $('#toggleSwitch').prop('checked', appStatus === 'on');
 
-    // Implement your voice command handling here
-
-    // For example:
-    if (transcript.includes('turn on') || transcript.includes('on')) {
-      toggleSwitch.checked = true;
-      toggleApp('on');
-    } else if (transcript.includes('turn off') || transcript.includes('off')) {
-      toggleSwitch.checked = false;
-      toggleApp('off');
-    } else if (transcript.includes('check status') || transcript.includes('app status') || transcript.includes('status')) {
-      checkStatus();
+    // Update body background color based on the switch status
+    if (appStatus === 'on') {
+      document.body.style.backgroundColor = 'yellow'; // Change to desired color
+    } else {
+      document.body.style.backgroundColor = 'grey'; // Change to desired color
     }
+  }
 
-    // Add more voice command handling as needed
-  };
+  // Listen for status updates
+  socket.on('statusUpdate', function (status) {
+    updateAppStatus(status);
+    clickSound.play(); // Play the click sound
+  });
 
-  // Function to toggle the switch based on voice commands
-  function toggleApp(state) {
+  $('#toggleSwitch').change(function () {
+    const isChecked = $(this).is(':checked');
+    const command = isChecked ? '1' : '0';
+
+    // Emit the toggleSwitch event to the server
+    socket.emit('toggleSwitch', isChecked);
+
+    $.post('/send-command', { command }, function (data) {
+      console.log(data);
+
+      // After sending the command, update app status and toggle switch
+      $.get('/app-status', function (data) {
+        updateAppStatus(data.status);
+      });
+    });
+  });
+
+  // Initial update
+  $.get('/app-status', function (data) {
+    updateAppStatus(data.status);
+  });
+
+  const toggleSwitch = document.getElementById('toggleSwitch');
+  const audio = document.getElementById('audio');
+
+  // Initialize the SpeechRecognition API
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+  recognition.continuous = true;
+
+  // Add a function to toggle the switch based on voice commands
+  const toggleApp = (state) => {
     const url = `https://mqttnodejs.glitch.me/toggle-app?state=${state}`;
     fetch(url)
       .then(response => {
@@ -92,16 +68,52 @@ $(document).ready(function () {
       })
       .then(data => {
         console.log(`Switch ${state} ho gaya hai`);
-        // Play click sound when the switch is toggled
-        speak(`Switch ${state} ho gaya hai ji`);
+        speak(`Switch ${state} ho gaya`);
         audio.play();
       })
       .catch(error => {
         console.error('Error:', error);
       });
-  }
+  };
 
-  // ...
+  // Add voice recognition for toggle commands
+  recognition.onresult = function (event) {
+    const transcript = event.results[event.results.length - 1][0].transcript.toLowerCase();
+    console.log('Voice input:', transcript);
+
+    if (transcript.includes('turn on') || transcript.includes('on')) {
+      toggleSwitch.checked = true;
+      toggleApp('on');
+    } else if (transcript.includes('turn off') || transcript.includes('off')) {
+      toggleSwitch.checked = false;
+      toggleApp('off');
+    } else if (transcript.includes('check status') || transcript.includes('app status') || transcript.includes('status')) {
+      checkStatus();
+    } else if (transcript.includes('developer')) {
+      speak('इस ऐप ka डेवलपर NIELIT ROPAR है');
+    } else if (transcript.includes('hello')) {
+      speak('हां भाई जी, बोलो?');
+    } else if (transcript.includes('how are you') || transcript.includes('kya haal hai')) {
+      speak('मैं ठीक हूं, धन्यवाद! आपका कैसे सहाय्य कर सकता हूं?');
+    } else if (transcript.includes('tell me a joke') || transcript.includes('joke')) {
+      speak('बिल्कुल! ये raha एक jabardast चुटकुला: क्या आपको पता है, एक जानवर होता है जो केवल बहुत अच्छे फोटोज़ खिचवाता है? वो aapp ho!');
+    } else if (transcript.includes('thank you')) {
+      speak('कोई बात नहीं, आपका स्वागत है!');
+    } else if (transcript.includes('smart')) {
+      speak('सबसे होशियार व्यक्ति तो आप हैं!');
+    } else if (transcript.includes('fact')) {
+      speak('जानकारी के लिए एक मजेदार तथ्य: कुछ बिल्लियां हाथी की तरह दौड़ सकती हैं!');
+    } else if (transcript.includes('life')) {
+      speak('जीवन का अर्थ है खुशियों और साझा करने में!');
+    } else if (transcript.includes('love')) {
+      speak('love you too');
+    } else if (transcript.includes('about you')) {
+      speak('I can turn on off switches anywhere from the world');
+    } else {
+      speak('मुझे माफ़ करें, मैं इस कमांड को समझ नहीं पाया।');
+    }
+  };
+
   // Function to check app status
   function checkStatus() {
     fetch('https://mqttnodejs.glitch.me/app-status')
@@ -113,9 +125,10 @@ $(document).ready(function () {
       })
       .then(data => {
         const appStatus = data.status;
+        const appStatusText = document.getElementById('appStatus');
         if (appStatusText) {
           appStatusText.textContent = `App Status: ${appStatus}`;
-          speak(`iss samay switch ka status ${appStatus} hai ji`);
+          speak(`iss samay switch ka status ${appStatus} hai`);
         } else {
           console.error('appStatusText element not found');
         }
@@ -124,7 +137,6 @@ $(document).ready(function () {
         console.error('Error:', error);
       });
   }
-// ...
 
   // Function to speak a message
   function speak(message) {
@@ -133,15 +145,29 @@ $(document).ready(function () {
     synth.speak(utterance);
   }
 
-  // Handle browser visibility changes
-  document.addEventListener('visibilitychange', () => {
-    if (document.hidden && isRecognitionActive) {
-      recognition.stop();
-    }
-  });
+  // Start continuous recognition
+  recognition.start();
 
-  // Initial update
-  $.get('/app-status', function (data) {
-    updateAppStatus(data.status);
+  // Function to start voice recognition
+  function startRecognition() {
+    recognition.start();
+    console.log('Voice recognition started');
+  }
+
+  // Add click event listener to the "Start Voice Recognition" button
+  const startButton = document.getElementById('startVoiceRecognition');
+  startButton.addEventListener('click', startRecognition);
+
+  // Add visibility change event listener to handle page visibility
+  document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+      // Handle page visibility when the page is hidden
+      recognition.stop();
+      console.log('Voice recognition stopped due to page visibility change');
+    } else {
+      // Handle page visibility when the page is visible again
+      recognition.start();
+      console.log('Voice recognition started as the page became visible');
+    }
   });
 });
